289b60dce3bc95e6696e7597c4287b77c64f9834
Linda Ho <linda.oanh.ho@gmail.com>
2017-11-17 10:21:08 -0600

    Add Gradle wrapper and intial card class with tests

---
+++ src/test/java/TestCard.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestCard {
+    private Card fixture;
+
+    @Before
+    public void setUp() {
+        fixture = new Card();
+    }
+
+    @After
+    public void tearDown() {
+        fixture = null;
+    }
+
+    @Test
+    public void testInitial() {
+        fixture.setQuestion("What is the capital of Illinois");
+        fixture.setAnswer("Springfield");
+        assertEquals("What is the capital of Illinois", fixture.getQuestion());
+        assertEquals("Springfield", fixture.getAnswer());
+    }
+
+    @Test
+    public void testCheckAnswer() {
+        fixture.setQuestion("What is the capital of Illinois");
+        fixture.setAnswer("Springfield");
+        assertEquals(false, fixture.getIsCorrect());
+        assertEquals(true, fixture.checkAnswer("Springfield"));
+        assertEquals(false, fixture.checkAnswer("Chicago"));
+    }
+
+}
