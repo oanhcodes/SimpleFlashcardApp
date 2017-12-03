5349692b85cee03d96d88029e2c7cccb82196edf
Eunice Montenegro <31542650+emontenegro410@users.noreply.github.com>
2017-12-03 16:53:00 -0600

    Clear deck class for merge

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,43 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+
 
 public class Deck {
 
-
-    private Stack<Card> theDeck;
-
-    public Deck() {
-        theDeck= new Stack<Card>();
-
-    }
-    //add single card to the deck
-    public void addCard(Card card){
-        theDeck.push(card);
-    }
-
-    // add multiple cards to the deck
-    public void addCards(ArrayList<Card> Cards){
-        for(int i=0; i<Cards.size(); i++){
-            addCard(Cards.get(i));
-        }
-    }
-
-    //retrieve single card from deck
-    public Card retrieveCard(){
-        return theDeck.pop();
-    }
-
-    public int getSize(){
-        return theDeck.size();
-    }
-
-    public Boolean checkComplete(){
-
-    }
-
-
-    public Card getCard(int index){
-        return theDeck.get(index);
-    }
 }
