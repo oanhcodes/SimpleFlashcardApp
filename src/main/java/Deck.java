7f3d7a6290008632ac1ff22ce38ef57afde495ba
Linda Ho <linda.oanh.ho@gmail.com>
2017-12-03 16:47:28 -0600

    Change deck to a queue implementation

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,37 +1,80 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Deck {
+public class Deck<E> implements SimpleQueue<E> {
 
+    private final int capacity;
+    private int size;
+    private int front;
+    private int rear;
+    private final E[] data;
+    private List<E> cardList;
 
-    private List<Card> theDeck;
-    private List<Card> incorrect;
-
-    public Deck() {
-        theDeck= new ArrayList<Card>();
-        incorrect= new ArrayList<Card>();
+    @SuppressWarnings("unchecked")
+    public Deck(final int capacity) {
+        this.capacity = capacity;
+        this.data = (E[]) new Object[capacity];
+        this.size = 0;
+        this.front = 0;
+        this.rear = capacity - 1;
     }
-    //add single card to the deck
-    public void addCard(Card card){
-        theDeck.add(card);
 
+    @Override
+    public boolean offer(final E obj) {
+        if (size < capacity) {
+            size++;
+            rear = (rear + 1) % capacity;
+            data[rear] = obj;
+            return true;
+        }
+        return false;
     }
-    // add multiple cards to the deck
-    public void addCards(ArrayList<Card> Cards){
-        for(int i=0; i<Cards.size(); i++){
-            addCard(Cards.get(i));
+
+    @Override
+    public E peek() {
+        if (size == 0) {
+            return null;
+        } else {
+            return data[front];
         }
     }
-    //remove single card from deck
-    public Card removeCard(int index){
-        return theDeck.remove(index);
+
+    @Override
+    public E poll() {
+        if (size == 0) {
+            return null;
+        } else {
+            E result = data[front];
+            front = (front + 1) % capacity;
+            size--;
+            return result;
+        }
     }
 
-    public int getSize(){
-        return theDeck.size();
+    @Override
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
     }
 
-    public Card getCard(int index){
-        return theDeck.get(index);
+    @Override
+    public int size() {
+        return size;
     }
+
+    @Override
+    public List<E> asList() {
+        // Done implement using an ArrayList preallocated with the right size
+        cardList = new ArrayList<>(capacity);
+        while (!isEmpty()) {
+            cardList.add(data[front]);
+            front = (front + 1) % capacity;
+            size--;
+        }
+        return cardList;
+    }
+
+
 }
