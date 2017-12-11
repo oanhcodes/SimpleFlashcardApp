8ac268c4a7691dadd801f23fed7c3fe564f17f2b
Linda Ho <linda.oanh.ho@gmail.com>
2017-12-10 21:07:11 -0600

    Add functionality to create a custom flashcard deck

--- src/main/java/FlashcardGame.java
+++ src/main/java/FlashcardGame.java
@@ -5,51 +5,74 @@ import java.util.Scanner;
 
 public class FlashcardGame {
 
-  // Deck instance that will be played
-  private static final SimpleQueue<Card> deckQueue = new Deck<>(10);
+  private static SimpleQueue<Card> deckQueue = new Deck<>(10);
   private static List<Card> cardList = new ArrayList<>();
+  private static Scanner input = new Scanner(System.in);
 
   public static void main(final String[] args) {
-    final Scanner input = new Scanner(System.in);
-
-    // Create new instance of flashcard game
+    // Create a new flashcard game
     FlashcardGame newGame = new FlashcardGame();
 
-    // Prepare a new deck of cards for the game
-    newGame.populateDeck();
-
-    // Create an empty scorecard map for this game
+    // Create a new scorecard
     Map<Card, Integer> theScoreCard = new HashMap<>();
     ScoreCard sc = new ScoreCard(theScoreCard);
-    sc.createNewScoreCard(cardList.iterator());
 
     // Start Flashcard Game
     System.out.println(
-        "Welcome to the flash card game. Let's practice your state and capitals. Type exit to leave the game early.\n");
+        "Welcome to the flash card game. Do you want to practice state capitals or create your own deck?");
+    System.out.println("1. State Capitals");
+    System.out.println("2. Create New Deck");
+
+    String selection = input.nextLine();
+
+    // Generate cards with fake data or user input
+    switch (selection) {
+      case "1":
+        newGame.createFakeCardData();
+        System.out.println(
+            "Let's practice your state capitals! Type 'exit' at any time to leave the game early.\n");
+        break;
+      case "2":
+        System.out.println("How many cards would you like to add to your custom deck?");
+        String newDeckSize = input.nextLine();
+        System.out.println("Ok. Let's create " + newDeckSize + " new cards.");
+        newGame.createNewDeck(Integer.parseInt(newDeckSize));
+        System.out.println(
+            "Let's practice your custom flashcards! Type 'exit' at any time to leave the game early.\n");
+        break;
+      default:
+        System.out.println("That is not an option. Exiting Game...");
+        System.exit(0);
+        break;
+    }
+
+    // Populate deck queue with the card list
+    newGame.populateDeck(cardList);
+    sc.createNewScoreCard(cardList.iterator());
 
     // Continue asking questions until all cards are answered correctly
     while (!deckQueue.isEmpty()) {
       Card currCard = newGame.playCard();
 
-      System.out.println("What is the capital of " + currCard.getQuestion() + "?");
+      System.out.println(currCard.getQuestion());
 
       // Retrieve and check answer
       String response = input.nextLine().toLowerCase();
-      if (currCard.checkAnswer(response)) {
-        // Add attempt in scorecard
-        sc.addAttempt(currCard);
-        System.out.println("Nice Job! You got that right.\n");
-      } else if (response.equals("exit")) {
+
+      if (response.equals("exit")) {
         System.out.println("Exiting Game...");
         System.exit(0);
+      } else if (currCard.checkAnswer(response)) {
+        sc.addAttempt(currCard);
+        System.out.println("Nice Job! You got that right.\n");
       } else {
-        // Add attempt in scorecard
         sc.addAttempt(currCard);
         System.out.println(
             "Sorry that was incorrect. This card has been added back to the deck.\n");
         newGame.replayCard(currCard);
       }
     }
+
     System.out.println("\nCongrats! You've finished answering all the questions correctly!");
 
     // Print score card
@@ -57,15 +80,11 @@ public class FlashcardGame {
   }
 
   /** Add all cards to the deck */
-  public void populateDeck() {
-    List<Card> cards = createFakeCardData();
+  public void populateDeck(List<Card> cards) {
     for (Card c : cards) {
       deckQueue.offer(c);
     }
   }
-
-  /** Create an empty scorecard */
-  public void createScoreCard(SimpleQueue<Card> currDeck) {}
 
   /** Retrieve a card from the deck */
   public Card playCard() {
@@ -79,16 +98,16 @@ public class FlashcardGame {
 
   /** Generate Fake Flashcard Data */
   public List<Card> createFakeCardData() {
-    Card a = new Card("California", "Sacramento");
-    Card b = new Card("Hawaii", "Honolulu");
-    Card c = new Card("Wisconsin", "Madison");
-    Card d = new Card("Illinois", "Springfield");
-    Card e = new Card("Ohio", "Columbus");
-    Card f = new Card("Arizona", "Phoenix");
-    Card g = new Card("Alabama", "Montgomery");
-    Card h = new Card("Florida", "Tallahassee");
-    Card i = new Card("Georgia", "Atlanta");
-    Card j = new Card("Tennessee", "Nashville");
+    Card a = new Card("What is the capital of California?", "Sacramento");
+    Card b = new Card("What is the capital of Hawaii?", "Honolulu");
+    Card c = new Card("What is the capital of Wisconsin?", "Madison");
+    Card d = new Card("What is the capital of Illinois?", "Springfield");
+    Card e = new Card("What is the capital of Ohio?", "Columbus");
+    Card f = new Card("What is the capital of Arizona?", "Phoenix");
+    Card g = new Card("What is the capital of Alabama?", "Montgomery");
+    Card h = new Card("What is the capital of Florida?", "Tallahassee");
+    Card i = new Card("What is the capital of Georgia?", "Atlanta");
+    Card j = new Card("What is the capital of Tennessee?", "Nashville");
 
     cardList.add(a);
     cardList.add(b);
@@ -102,5 +121,21 @@ public class FlashcardGame {
     cardList.add(j);
 
     return cardList;
+  }
+
+  public void createNewDeck(int size) {
+    deckQueue = new Deck<>(size);
+    for (int i = 0; i < size; i++) {
+      createNewCard();
+    }
+  }
+
+  public void createNewCard() {
+    System.out.println("Enter a question");
+    String question = input.nextLine();
+    System.out.println("Enter the answer");
+    String answer = input.nextLine();
+    Card c = new Card(question, answer);
+    cardList.add(c);
   }
 }
